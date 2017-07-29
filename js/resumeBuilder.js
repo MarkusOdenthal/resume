39c8fe6f04e34055ce66dcc4bdb7dcc4543392fa
markusodenthal <markus.odenthal@outlook.de>
2017-07-29 08:53:02 +0200

    Add my name and role to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Markus Odenthal";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Student in Sustainable Electrical Power Supply";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+// var awesomeThoughts = "I am Markus and I am AWESOME!";
+
+// var funThoughts = awesomeThoughts.replace(["AWESOME"], ["FUN"])
+
+// $("body").append(funThoughts);
+
+// console.log(awesomeThoughts);
+
